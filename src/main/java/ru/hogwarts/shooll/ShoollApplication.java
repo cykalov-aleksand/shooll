65105193065ef71093b93d4bbb644de22a0adbb8
1в <cykalov.aleksand@mail.ru>
2025-03-18 22:29:06 +0300

    Initiallise Commit

---
+++ src/main/java/ru/hogwarts/shooll/ShoollApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.shooll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/shooll/ShoollApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.shooll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
