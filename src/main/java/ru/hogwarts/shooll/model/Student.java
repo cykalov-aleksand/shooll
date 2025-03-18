0d445d27baa152c2aa03033f3da1fcef115237ba
1в <cykalov.aleksand@mail.ru>
2025-03-18 23:06:51 +0300

    Initiallise Commit

---
+++ src/main/java/ru/hogwarts/shooll/model/Student.java
@@ -0,0 +1,61 @@
+package ru.hogwarts.shooll.model;
+
+import java.util.Objects;
+
+public class Student {
+    private long id;
+    private String name;
+    private int age;
+
+    public Student(long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Student student = (Student) object;
+        return id == student.id && age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
