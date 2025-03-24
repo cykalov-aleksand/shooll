e51e6e54dcc9a2925e8b78d6f65ce4dd05bc25b5
1в <cykalov.aleksand@mail.ru>
2025-03-24 19:07:38 +0300

    Внес исправления, проверьте пожалуйста домашнее задание

--- src/main/java/ru/hogwarts/shooll/Scripts.sql
+++ src/main/java/ru/hogwarts/shooll/Scripts.sql
@@ -6,8 +6,9 @@ SELECT name FROM student
 SELECT * FROM student
 WHERE name LIKE '%а%'
 
-SELECT * FROM  student
-WHERE age < 20
+SELECT * FROM student
+WHERE age<20
 
 SELECT * FROM student
 ORDER BY age ASC
+
