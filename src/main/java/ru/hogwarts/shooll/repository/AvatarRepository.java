40248b208a9f7da9fc5e42df57628870c1102cf1
1в <cykalov.aleksand@mail.ru>
2025-04-05 17:21:44 +0300

    проверьте пожалуйста домашнее задание

---
+++ src/main/java/ru/hogwarts/shooll/repository/AvatarRepository.java
@@ -0,0 +1,11 @@
+package ru.hogwarts.shooll.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.hogwarts.shooll.model.Avatar;
+
+
+import java.util.Optional;
+
+public interface AvatarRepository extends JpaRepository<Avatar,Long> {
+    Optional<Avatar> findByStudentId(long studentId);
+}
